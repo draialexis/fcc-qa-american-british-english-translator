e3e1d6ccee1e00219ec80481caf38c52c4f5dd17
Alexis Drai <alexis@alexisdrai.com>
2024-08-27 20:51:51 +0200

    Add failing API tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,13 +8,64 @@ chai.use(chaiHttp);
 let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
-// TODO write the tests in this list
-/*
- Translation with text and locale fields: POST request to /api/translate
- Translation with text and invalid locale field: POST request to /api/translate
- Translation with missing text field: POST request to /api/translate
- Translation with missing locale field: POST request to /api/translate
- Translation with empty text: POST request to /api/translate
- Translation with text that needs no translation: POST request to /api/translate
- */
+  const translator = new Translator();
+
+  test('Translation with text and locale fields: POST request to /api/translate', function(done) {
+    chai.request(server).post('/api/translate').send({
+      text: 'Mangoes are my favorite fruit.',
+      locale: 'american-to-british',
+    }).end(function(err, res) {
+      assert.equal(res.body.translation, 'Mangoes are my <span class="highlight">favourite</span> fruit.');
+      done();
+    });
+  });
+
+  test('Translation with text and invalid locale field: POST request to /api/translate', function(done) {
+    chai.request(server).post('/api/translate').send({
+      text: 'Mangoes are my favorite fruit.',
+      locale: 'invalid-locale',
+    }).end(function(err, res) {
+      assert.equal(res.body.error, 'Invalid value for locale field');
+      done();
+    });
+  });
+
+  test('Translation with missing text field: POST request to /api/translate', function(done) {
+    chai.request(server).post('/api/translate').send({
+      locale: 'american-to-british',
+    }).end(function(err, res) {
+      assert.equal(res.body.error, 'Required field(s) missing');
+      done();
+    });
+  });
+
+  test('Translation with missing locale field: POST request to /api/translate', function(done) {
+    chai.request(server).post('/api/translate').send({
+      text: 'Mangoes are my favorite fruit.',
+    }).end(function(err, res) {
+      assert.equal(res.body.error, 'Required field(s) missing');
+      done();
+    });
+  });
+
+  test('Translation with empty text: POST request to /api/translate', function(done) {
+    chai.request(server).post('/api/translate').send({
+      text: '',
+      locale: 'american-to-british',
+    }).end(function(err, res) {
+      assert.equal(res.body.error, 'No text to translate');
+      done();
+    });
+  });
+
+  test('Translation with text that needs no translation: POST request to /api/translate', function(done) {
+    chai.request(server).post('/api/translate').send({
+      text: 'Bleep bloop.',
+      locale: 'british-to-american',
+    }).end(function(err, res) {
+      assert.equal(res.body.translation, 'Everything looks good to me!');
+      done();
+    });
+  });
 });
+
